ce272cd24084df95f3ea6bebe93f9f8c60f9b329
Williaml9329 <62862045+Williaml9329@users.noreply.github.com>
2021-07-16 13:31:01 -0400

    Add files via upload

---
+++ src/my/pomodoroclock/PomodoroClock.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package my.pomodoroclock;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import java.awt.*;
+
+/**
+ *
+ * @author James's Desktop
+ */
+public class PomodoroClock extends PomodoroUI {
+   
+    public void Main(String[] args) {
+    
+        System.out.print("\007"); // \007 is the ASCII bell
+       System.out.flush();
+    
+    }
+    
+}
